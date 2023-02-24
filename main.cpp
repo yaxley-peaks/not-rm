f195f4dcb2fa056dc289ce9b4d85c631636caf0b
yaxley-peaks <epiclycoolgaemer@gmail.com>
2023-02-24 17:38:43 +0530

    Reformatted C++ file.
    Clion formatting sucks lmao

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
-#include <iostream>
-#include <fstream>
+/*
+This file is merely proof of concept and not feature complete. Do not use this
+to do any serious things. Use the rust version instead. This file is not
+maintained.
+*/
+
 #include <filesystem>
+#include <fstream>
+#include <iostream>
 #include <string>
 #include <vector>
 
@@ -8,17 +14,17 @@ namespace fs = std::filesystem;
 
 typedef std::vector<std::string> str_vec;
 
-str_vec get_all_files(const std::string &path) {
+str_vec get_all_files(const std::string& path) {
     str_vec res = str_vec();
-    for(const fs::directory_entry& entry: fs::directory_iterator(path)){
+    for (const fs::directory_entry& entry : fs::directory_iterator(path)) {
         res.push_back(entry.path().string());
     }
     return res;
 }
 
-str_vec filter_not_paths(const str_vec& paths, const std::string& pat){
+str_vec filter_not_paths(const str_vec& paths, const std::string& pat) {
     str_vec res = str_vec();
-    for (const std::string &item: paths)
+    for (const std::string& item : paths)
         if (item.find(pat) != std::string::npos) res.push_back(item);
     return res;
 }
@@ -28,12 +34,13 @@ struct CLI {
     std::string pat;
 };
 
-std::unique_ptr<CLI> handle_args(char** args, int n){
+std::unique_ptr<CLI> handle_args(char** args, int n) {
     CLI cli = {};
     switch (n) {
         case 1:
         case 2:
-            std::cerr << "error: The following required arguments were not provided:\n"
+            std::cerr << "error: The following required arguments were not "
+                         "provided:\n"
                          "    <INPUT>\n"
                          "    <EXT>\n"
                          "\n"
@@ -42,7 +49,7 @@ std::unique_ptr<CLI> handle_args(char** args, int n){
                          "    not-rm.exe <INPUT> <EXT>\n"
                          "\n"
                          "For more information try --help"
-                     <<   std::endl;
+                      << std::endl;
             exit(-1);
         case 3:
             cli.input = std::string(args[1]);
@@ -58,11 +65,12 @@ int main(int argc, char** argv) {
     std::cout << args.input << " -> " << args.pat << '\n';
     auto notPaths = filter_not_paths(get_all_files(args.input), args.pat);
 
-    for (const std::string& notPath: notPaths) {
+    for (const std::string& notPath : notPaths) {
         std::cout << notPath << '\n';
         fs::path path_rm = notPath;
-        if(fs::is_directory(path_rm)) continue;
-        if(!fs::remove(path_rm)) std::cout << "Could not remove: " << path_rm << '\n';
+        if (fs::is_directory(path_rm)) continue;
+        if (!fs::remove(path_rm))
+            std::cout << "Could not remove: " << path_rm << '\n';
     }
     return 0;
 }
